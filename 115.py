499a8a5fd3ee086fdf6689db3d6352183ec64c35
kikyous <kikyous@163.com>
2012-08-21 11:52:31 +0800

    log to file and windows compatible

--- 115.py
+++ 115.py
@@ -1,24 +1,36 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 accounts_here = [
-    {"account":'115115115','password':'000000'},
-    {"account":'116116116','password':'000000'},
+    # {"account":'115115115','password':'000000'},
+    # {"account":'116116116','password':'000000'},
+    {"account":'18909444967','password':'11352355'},
+    {"account":'826123231@qq.com','password':'826123231'},
 ]
 
 import urllib
 import urllib2
 import cookielib
 import json
-import re,time
+import re,time,os
+class Log:
+  def __init__(self):
+    self.PATH=os.path.abspath(os.path.expanduser('.'))
+    self.fd=open(self.PATH+"/log.txt",'a')
+    t=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    self.log("# %s"%t,False)
+  def log(self,s,indent=True):
+    if indent:
+      s="  %s"%s
+    print(s.decode("u8"))
+    self.fd.write(s)
+    self.fd.write("\n")
 
 class Here115:
   def __init__(self):
-    t=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    print(t)
     cj = cookielib.CookieJar()
     self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
     urllib2.install_opener(self.opener)
-    self.opener.addheaders = [('User-agent', 'IE')]
+    self.opener.addheaders = [('User-agent', 'Mozilla/5.0 (X11; Linux i686)')]
 
   def login(self, username, password):
     url = 'https://passport.115.com/?ac=login'
@@ -27,15 +39,15 @@ class Here115:
     try:
       fd = self.opener.open(req)
     except Exception, e:
-      print('网络连接错误！')
+      l.log('网络连接错误！')
       return False
     res=fd.read()
     fd.close()
     if re.search('location\.href="http://115.com"', res) == None:
-      print('%s 密码不正确！\n' % username)
+      l.log('%s 密码不正确！\n' % username)
       return False
     else:
-      print('%s 登陆成功，准备摇奖..   ' % username),
+      l.log('%s 登陆成功，准备摇奖..   ' % username),
       return True
 
   def pick_space(self):
@@ -46,7 +58,7 @@ class Here115:
     fd.close()
     token = re.search("take_token:\s'(\w+)'", token_page)
     if not token:
-      print('今天已经摇过了...')
+      l.log('今天已经摇过了...')
       return
 
     url = 'http://115.com/?ct=ajax_user&ac=pick_space&token=' + token.group(1)
@@ -55,15 +67,16 @@ class Here115:
     res_json = json.loads(fd.read())
     fd.close()
     if res_json['state'] == False:
-      print('摇奖失败！')
+      l.log('摇奖失败！')
       return
     str=u'\n获取空间：%s, 总空间：%s, 已使用：%s, 获取雨露：%d\n' % (res_json['picked'], res_json['total_size'], res_json['used_percent'], res_json['exp'])
-    print str.encode("u8")
+    l.log(str.encode("u8"))
 
 
 if __name__ == '__main__':
-  h = Here115()
+  l=Log()
   for i in accounts_here:
+    h = Here115()
     if not h.login(i['account'],i['password']):
       continue
     h.pick_space()
